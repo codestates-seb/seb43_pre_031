e0b7bf88662e6dea3a796f545da7267b383fcd07
Hyun Jin, Lee <nagi5924@gmail.com>
2023-04-17 14:59:11 +0900

    [Design] Social login button colored

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -38,15 +38,9 @@ export default function Login() {
             src="/assets/128px-Stack_Overflow_icon.svg.png"
             alt="stackoverflow logo"
           />
-          <BtnContainer>
-            <Btn>
-              <BtnIcon
-                aria-hidden="true"
-                className="native svg-icon iconGoogle"
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-              >
+          <SCBtnContainer>
+            <SCBtn>
+              <SCBtnIcon aria-hidden="true" viewBox="0 0 18 18">
                 <path
                   fill="#4285F4"
                   d="M16.51 8H8.98v3h4.3c-.18 1-.74 1.48-1.6 2.04v2.01h2.6a7.8 7.8 0 0 0 2.38-5.88c0-.57-.05-.66-.15-1.18Z"
@@ -63,40 +57,28 @@ export default function Login() {
                   fill="#EA4335"
                   d="M8.98 4.18c1.17 0 2.23.4 3.06 1.2l2.3-2.3A8 8 0 0 0 1.83 5.4L4.5 7.49a4.77 4.77 0 0 1 4.48-3.3Z"
                 ></path>
-              </BtnIcon>
+              </SCBtnIcon>
               Log in with Google
-            </Btn>
-            <Btn>
-              <BtnIcon
-                aria-hidden="true"
-                className="svg-icon iconGitHub"
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-              >
+            </SCBtn>
+            <SCBtn>
+              <SCBtnIcon aria-hidden="true" viewBox="0 0 18 18">
                 <path
-                  fill="#010101"
+                  fill="#fff"
                   d="M9 1a8 8 0 0 0-2.53 15.59c.4.07.55-.17.55-.38l-.01-1.49c-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82a7.42 7.42 0 0 1 4 0c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48l-.01 2.2c0 .21.15.46.55.38A8.01 8.01 0 0 0 9 1Z"
                 ></path>
-              </BtnIcon>
+              </SCBtnIcon>
               Log in with Github
-            </Btn>
-            <Btn>
-              <BtnIcon
-                aria-hidden="true"
-                className="svg-icon iconFacebook"
-                width="18"
-                height="18"
-                viewBox="0 0 18 18"
-              >
+            </SCBtn>
+            <SCBtn>
+              <SCBtnIcon aria-hidden="true" viewBox="0 0 18 18">
                 <path
-                  fill="#4167B2"
+                  fill="#fff"
                   d="M3 1a2 2 0 0 0-2 2v12c0 1.1.9 2 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H3Zm6.55 16v-6.2H7.46V8.4h2.09V6.61c0-2.07 1.26-3.2 3.1-3.2.88 0 1.64.07 1.87.1v2.16h-1.29c-1 0-1.19.48-1.19 1.18V8.4h2.39l-.31 2.42h-2.08V17h-2.5Z"
                 ></path>
-              </BtnIcon>
+              </SCBtnIcon>
               Log in with Facebook
-            </Btn>
-          </BtnContainer>
+            </SCBtn>
+          </SCBtnContainer>
         </SocialContainer>
         <FormContainer>
           <form onSubmit={onSubmitHandler}>
@@ -190,18 +172,31 @@ const PwBox = styled.div`
   justify-content: space-between;
 `;
 
-const BtnContainer = styled.div`
+const SCBtnContainer = styled.div`
   width: 100%;
 `;
-const Btn = styled.button`
+const SCBtn = styled.button`
   width: 100%;
   padding: 10px;
   margin: 4px 0;
   text-align: center;
   display: block;
   border: 1px solid lightgray;
+  color: #fff;
+  &:first-child {
+    background-color: #fff;
+    color: ${(props) => props.theme.color.black900};
+  }
+  &:nth-child(2) {
+    background-color: ${(props) => props.theme.color.black900};
+  }
+  &:last-child {
+    background-color: ${(props) => props.theme.color.blue900};
+  }
 `;
-const BtnIcon = styled.svg`
+const SCBtnIcon = styled.svg`
+  width: 18px;
+  height: 18px;
   vertical-align: baseline;
   margin-top: -0.3em;
   margin-bottom: -0.3em;
