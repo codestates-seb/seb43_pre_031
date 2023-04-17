bb7cc3f32cf093365f22c500c44377c3c3ab909e
Hyun Jin, Lee <nagi5924@gmail.com>
2023-04-17 14:43:50 +0900

    [Design] Adjust Globalstyle, etc

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -2,64 +2,16 @@ import { useState } from 'react';
 import styled from 'styled-components';
 // import axios from 'axios';
 // import { useDispatch } from 'react-redux';
+import Button from '../elements/Button';
+// import Theme from '../utils/Theme';
 
-const Main = styled.main`
-  margin: 2rem;
-`;
-const SocialContainer = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 6px;
-`;
-const FormContainer = styled.section`
-  padding: 24px;
-  margin-bottom: 24px;
-  background-color: #fff;
-  border-radius: 8px;
-  box-shadow: 0 0 24px rgba(0, 0, 0, 0.1);
-`;
-const InputBox = styled.div`
-  margin: 6px 0;
-`;
-
-const BtnContainer = styled.div`
-  width: 100%;
-`;
-const Btn = styled.button`
-  width: 100%;
-  padding: 10px;
-  margin: 4px 0;
-  text-align: center;
-  display: block;
-  border: 1px solid lightgray;
-`;
-const BtnIcon = styled.svg`
-  vertical-align: baseline;
-  margin-top: -0.3em;
-  margin-bottom: -0.3em;
-  margin-right: 0.3rem;
-`;
-const LogoBox = styled.img`
-  width: 37px;
-  margin-bottom: 24px;
-`;
-const LoginBtn = styled.button`
-  width: 100%;
-  padding: 10px;
-  margin: 4px 0;
-  text-align: center;
-  display: block;
-  border: 1px solid lightgray;
-  background-color: #0a95ff;
-  color: #fff;
-`;
 export default function Login() {
   // useEffect(() => {
   //   axios.get('/user').then((response) => console.log(response.data));
   // }, []);
   // const dispatch = useDispatch();
 
+  // 회원 정보 보내기 (Create)
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
@@ -77,6 +29,7 @@ export default function Login() {
     };
     console.log(body);
   };
+
   return (
     <>
       <Main>
@@ -148,37 +101,134 @@ export default function Login() {
         <FormContainer>
           <form onSubmit={onSubmitHandler}>
             <InputBox>
-              <label htmlFor="email">Email</label>
-              <input
+              <Label htmlFor="email">Email</Label>
+              <Input
                 type="email"
                 id="email"
                 value={email}
                 onChange={onEmailHandler}
               />
-              <p>error</p>
+              <ErrorMsg>The email is not a valid email address.</ErrorMsg>
+              <ErrorMsg>The email or password is incorrect.</ErrorMsg>
             </InputBox>
-            <div>
-              <label htmlFor="password">Password</label>
-              <span>Forgot password?</span>
-              <input
+            <PwContainer>
+              <PwBox>
+                <Label htmlFor="password">Password</Label>
+                <Span>Forgot password?</Span>
+              </PwBox>
+              <Input
                 type="password"
                 id="password"
                 value={password}
                 onChange={onPwHandler}
               />
-              <p>error</p>
-              <p>
-                Passwords must contain at least eight characters, including at
-                least 1 letter and 1 number.
-              </p>
-            </div>
-            <LoginBtn>Log in</LoginBtn>
+            </PwContainer>
+            <LoginBtn text={'Log in btn'} />
           </form>
         </FormContainer>
-        <div>
-          Don’t have an account? <a href="/">Sign up</a>
-        </div>
+        <LinkContainer>
+          <div>
+            Don’t have an account? <a href="/">Sign up</a>
+          </div>
+          <div>
+            Are you an employer?{' '}
+            <a href="/">
+              Sign up on Talent
+              <svg
+                aria-hidden="true"
+                className="va-text-bottom sm:d-none svg-icon iconShareSm"
+                width="10"
+                height="10"
+                viewBox="0 0 14 14"
+              >
+                <path d="M5 1H3a2 2 0 0 0-2 2v8c0 1.1.9 2 2 2h8a2 2 0 0 0 2-2V9h-2v2H3V3h2V1Zm2 0h6v6h-2V4.5L6.5 9 5 7.5 9.5 3H7V1Z"></path>
+              </svg>
+            </a>
+          </div>
+        </LinkContainer>
       </Main>
     </>
   );
 }
+
+// Styled-components
+const LoginBtn = styled(Button)`
+  //안먹히는 것 같다?
+`;
+const Main = styled.main`
+  margin: 2rem;
+  padding: 24px;
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+`;
+const SocialContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 6px;
+`;
+const FormContainer = styled.section`
+  padding: 24px;
+  margin-bottom: 24px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 0 24px rgba(0, 0, 0, 0.1);
+`;
+const InputBox = styled.div`
+  margin: 6px 0;
+`;
+const Label = styled.label`
+  font-weight: bold;
+  font-size: 1.5rem;
+  margin: 2px 0;
+  padding: 0 2px;
+`;
+const PwContainer = styled.div`
+  margin-bottom: 24px;
+`;
+const PwBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const BtnContainer = styled.div`
+  width: 100%;
+`;
+const Btn = styled.button`
+  width: 100%;
+  padding: 10px;
+  margin: 4px 0;
+  text-align: center;
+  display: block;
+  border: 1px solid lightgray;
+`;
+const BtnIcon = styled.svg`
+  vertical-align: baseline;
+  margin-top: -0.3em;
+  margin-bottom: -0.3em;
+  margin-right: 0.3rem;
+`;
+const LogoBox = styled.img`
+  width: 37px;
+  margin-bottom: 24px;
+`;
+const Input = styled.input`
+  display: block;
+  width: 100%;
+  padding: 8px;
+  &:focus {
+    outline: ${(props) => props.theme.color.blue100} solid 0.4rem;
+  }
+`;
+const Span = styled.span`
+  font-size: 1rem;
+  color: ${(props) => props.theme.color.blue600};
+`;
+const ErrorMsg = styled.div`
+  color: red;
+`;
+const LinkContainer = styled.div`
+  text-align: center;
+`;
