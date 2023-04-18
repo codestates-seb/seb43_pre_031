5491ffdc2521c3515b6e783d194aa6261a03c7d0
JSooCha <chajinsoo14@gmail.com>
2023-04-18 09:21:21 +0900

    [Feat] 답변 CRUD 및 API 문서화

--- server/src/main/java/com/example/demo/answer/AnswerRepository.java
+++ server/src/main/java/com/example/demo/answer/AnswerRepository.java
@@ -1,2 +1,9 @@
-package com.example.demo.answer;public class AnswerRepository {
+package com.example.demo.answer;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    Optional<Answer> findById(Long id);
 }
