0add2ee0fe3283e024dfed3ce607f9c8052891c3
rsuubinn <rsuubinn@gmail.com>
2023-04-18 14:33:57 +0900

    [Feat] Aside 구현

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,10 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import SearchIcon from '@mui/icons-material/Search';
+import Button from '../elements/Button';
 
 const Container = styled.div`
+  z-index: 1;
   grid-area: header;
   position: fixed;
   top: 0;
@@ -11,39 +13,48 @@ const Container = styled.div`
   height: 6rem;
   background-color: ${(props) => props.theme.color.lightGray};
   display: flex;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
-  padding: 0 25rem;
   font-size: 1.5rem;
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05),
     0 2px 8px hsla(0, 0%, 0%, 0.05);
   border-top: 0.5rem solid ${(props) => props.theme.color.orange500};
   a {
+    margin: 0.5rem;
+    padding: 0.5rem 1rem;
     text-decoration: none;
     color: ${(props) => props.theme.color.black700};
+    &:hover {
+      background-color: ${(props) => props.theme.color.black100};
+      border-radius: 25px;
+    }
   }
   .right {
     display: flex;
     align-items: center;
   }
   button {
+    width: 7rem;
     padding: 1rem;
     border: 1px solid ${(props) => props.theme.color.blue400};
     border-radius: 3px;
     margin-left: 0.5rem;
+    cursor: pointer;
   }
   .login {
     background-color: ${(props) => props.theme.color.blue100};
     color: ${(props) => props.theme.color.blue400};
   }
-  .signup {
+  /* .signup {
     background-color: ${(props) => props.theme.color.blue400};
     color: white;
-  }
+  } */
 `;
 
 const Logo = styled.img`
   width: 16rem;
+  cursor: pointer;
+  margin-right: 2rem;
 `;
 
 const Search = styled.div`
@@ -55,6 +66,7 @@ const Search = styled.div`
   display: flex;
   align-items: center;
   margin-right: 0.5rem;
+  margin-left: 2rem;
   input {
     border: none;
     background-color: transparent;
@@ -72,9 +84,10 @@ const Search = styled.div`
 `;
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
     <Container>
-      <Logo src="/assets/logo.png" alt="logo" />
+      <Logo src="/assets/logo.png" alt="logo" onClick={() => navigate('/')} />
       <Link to="/about">About</Link>
       <Link to="/products">Products</Link>
       <Link to="/teams">For Teams</Link>
@@ -84,7 +97,8 @@ const Header = () => {
           <input type="text" placeholder="Search..." />
         </Search>
         <button className="login">Log in</button>
-        <button className="signup">Sign up</button>
+        <Button text="Sign up" />
+        {/* <button className="signup">Sign up</button> */}
       </div>
     </Container>
   );
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,6 +1,6 @@
 import Nav from '../components/Nav';
 import Header from '../components/Header';
-import Content from '../components/Content';
+import Questions from '../components/Questions';
 import Aside from '../components/Aside';
 import Footer from '../components/Footer';
 import styled from 'styled-components';
@@ -8,7 +8,7 @@ import styled from 'styled-components';
 const Container = styled.div`
   display: grid;
   margin: 0 auto;
-  grid-template-columns: 1fr 1fr 3fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 3.5fr 1.5fr 1fr;
   grid-template-areas:
     'header header header header header'
     '. nav main aside .'
@@ -20,7 +20,7 @@ const Main = () => {
     <Container>
       <Header />
       <Nav />
-      <Content />
+      <Questions />
       <Aside />
       <Footer />
     </Container>
