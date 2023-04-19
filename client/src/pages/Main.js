73dd3e5c33a1c40bf9298db241c8b57a3d8f9205
idx123 <jinhalee020@gmail.com>
2023-04-19 10:29:26 +0900

    [Comment] 임시 주석 처리

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,29 +1,33 @@
-import Nav from '../components/Nav';
-import Header from '../components/Header';
+// import Nav from '../components/Nav';
+// import Header from '../components/Header';
+// import Footer from '../components/Footer';
+// import styled from 'styled-components';
 import Questions from '../components/Questions';
 import Aside from '../components/Aside';
-import Footer from '../components/Footer';
-import styled from 'styled-components';
 
-const Container = styled.div`
-  display: grid;
-  margin: 0 auto;
-  grid-template-columns: 1fr 1fr 3.5fr 1.5fr 1fr;
-  grid-template-areas:
-    'header header header header header'
-    '. nav main aside .'
-    'footer footer footer footer footer';
-`;
+// const Container = styled.div`
+//   display: grid;
+//   margin: 0 auto;
+//   grid-template-columns: 1fr 1fr 3.5fr 1.5fr 1fr;
+//   grid-template-areas:
+//     'header header header header header'
+//     '. nav main aside .'
+//     'footer footer footer footer footer';
+// `;
 
 const Main = () => {
   return (
-    <Container>
-      <Header />
-      <Nav />
+    <div>
       <Questions />
       <Aside />
-      <Footer />
-    </Container>
+    </div>
+    // <Container>
+    //   <Header />
+    //   <Nav />
+    //   <Questions />
+    //   <Aside />
+    //   <Footer />
+    // </Container>
   );
 };
 
