c83f18ee20b686c61f0bbbad85b09398293f36a8
Hyun Jin, Lee <nagi5924@gmail.com>
2023-04-20 10:32:33 +0900

    [Fix] pull dev 3

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import Questions from '../components/Questions';
 
 const Container = styled.div``;
->>>>>>>>> Temporary merge branch 2
 
 const Main = ({ questions }) => {
   return (
