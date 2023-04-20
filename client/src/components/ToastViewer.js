55ce5adee2e9cff2b2e9cba97132bcba90ec2c26
idx123 <jinhalee020@gmail.com>
2023-04-20 23:46:28 +0900

    [Remove] 새로운 에디터 적용을 위한 삭제

--- client/src/components/ToastViewer.js
+++
@@ -1,8 +0,0 @@
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
-import { Viewer } from '@toast-ui/react-editor';
-
-const ToastViewer = ({ content }) => {
-  return <Viewer initialValue={content} />;
-};
-
-export default ToastViewer;
